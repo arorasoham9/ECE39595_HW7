644ef0e27fb72d507546a0de9fab6006e10366e7
arorasoham9 <68672198+arorasoham9@users.noreply.github.com>
2022-10-13 07:21:59 -0400

    asa

--- arora106/Array2D.cpp
+++ arora106/Array2D.cpp
@@ -5,7 +5,6 @@
 // This constructor is delegated to by other constructors and does all of the 
 // allocation, showing an example of a delegating constructor..  
 Array2D::Array2D(int numR, int numC, char differentiator) : numRows(numR), numCols(numC) {
-
    ary = new int*[numRows];
    for (int i = 0; i < numRows; i++) {
       ary[i] = new int[numCols];
@@ -39,7 +38,7 @@ int Array2D::operator( )(int row, int col) const {
    return ary[row][col];
 }
 
-bool Array2D::sameSize(const Array2D& other) const {
+bool Array2D::sameSize(const Array2D& other)const{
    return (numRows == other.getNumRows( ) && numCols == other.getNumCols( ));
 }
 
@@ -48,4 +47,82 @@ void Array2D::deleteAry(int** ary) {
       delete [ ] ary[i];
    }
    delete [ ] ary;
+}
+
+int Array2D::getNumRows() const{
+   return numRows;
+}
+
+int Array2D::getNumCols()const {
+   return numCols;
+}
+
+std::ostream& operator<<(std::ostream& os, Array2D& array){
+   for(int i = 0; i < array.getNumRows(); ++i){
+      for(int j = 0; j < array.getNumCols(); ++j){
+         os<<" "<<array.ary[i][j];
+      }
+      os <<endl;
+   }
+   return os;
+}
+
+Array2D Array2D::operator*( const Array2D& array ){
+   //more mem being allocated here
+   
+   if (numCols != array.getNumRows()){
+      Array2D arr(numRows, numCols, -1);
+      return arr;
+   }
+   else{
+      Array2D arr(numRows, array.getNumCols(), 'c');
+
+
+
+      for(int i = 0; i < numRows; ++i){
+        for(int j = 0; j < array.getNumCols(); ++j){
+            for(int k = 0; k < array.getNumRows(); ++k)
+            {
+               arr.ary[i][j] += ary[i][k] * array.ary[k][j];
+            }
+        }
+      }
+      return arr;
+   }
+}
+
+bool Array2D::operator==( const Array2D& array ){
+   if (!sameSize(array)) return false;
+   for (int i = 0; i < numRows; i++) {
+      for (int j = 0; j < numCols; j++) {
+         if (ary[i][j] != array.ary[i][j]){
+            return false;
+         }
+      }
+   }
+   return true;
+}
+
+
+Array2D& Array2D::operator= ( const Array2D& array ){
+   for (int i = 0; i < numRows; i++) {
+      for (int j = 0; j < numCols; j++) {
+         ary[i][j] = array.ary[i][j];
+      }
+   }
+   return *this;
+}
+
+
+
+Array2D::~Array2D(){
+
+}
+
+Array2D::Array2D(const Array2D& other): Array2D(other.getNumRows(), other.getNumCols(), 'c'){
+   for (int i = 0; i < other.getNumRows(); i++) {
+      for (int j = 0; j < other.getNumCols(); j++) {
+         ary[i][j] = other.ary[i][j];
+      }
+   }
 }
--- arora106/Array2D.h
+++ arora106/Array2D.h
@@ -1,6 +1,9 @@
 #ifndef ARRAY2D_H_
 #define ARRAY2D_H_
 #include <iostream>
+using namespace std;
+
+
 class Array2D {
 
 public:
@@ -10,10 +13,23 @@ public:
 
    virtual int& operator( )(int row, int col);
    virtual int operator( )(int row, int col) const;
+   int getNumRows()const;
+   int getNumCols()const;
+
+   
+   virtual Array2D operator*( const Array2D& array );
+   virtual bool operator==( const Array2D& array );
+   virtual Array2D& operator= ( const Array2D& array );
+   virtual ~Array2D();
+   // Array2D(const Array2D& other);
+   int** ary;
 
 private:
    Array2D(int numR, int numC, char differentiator);
    void deleteAry(int** ary);
-   bool sameSize(const Array2D& other) const;
+   bool sameSize(const Array2D& other)const;
+   int numRows;
+   int numCols;
 };
+std::ostream& operator<<(std::ostream& os, Array2D& array);
 #endif /* ARRAY2D_H_ */
